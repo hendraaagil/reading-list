e1bcf9aa0bb901f7265c05fb8721a5049c53ffad
hendra-agil <agilsyaputra26@gmail.com>
2020-09-19 21:57:29 +0700

    🔨 BookDetails & BookList components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import BookContextProvider from './contexts/BookContext';
 import Navbar from './components/Navbar';
+import BookList from './components/BookList';
 
 function App() {
   return (
     <div className="App">
       <BookContextProvider>
         <Navbar />
+        <BookList />
       </BookContextProvider>
     </div>
   );
---
+++ src/components/BookList.js
@@ -0,0 +1,21 @@
+import React, { useContext } from 'react';
+import { BookContext } from '../contexts/BookContext';
+import BookDetails from './BookDetails';
+
+const BookList = () => {
+  const { books } = useContext(BookContext);
+
+  return books.length ? (
+    <div className="book-list">
+      <ul>
+        {books.map((book) => {
+          return <BookDetails book={book} key={book.id} />;
+        })}
+      </ul>
+    </div>
+  ) : (
+    <div className="empty">No books to read. Hello free time 🙂</div>
+  );
+};
+
+export default BookList;
